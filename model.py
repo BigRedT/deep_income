7f794793d703dce690d120f413f279ccf709ee13
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-09 16:38:05 -0500

    add model

---
+++ model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class IncomeClassifier(nn.Module):
+    def __init__(self,in_dim,hidden_dim=None):
+        super().__init__()
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.out_dim = 2 
+        self.layers = nn.Sequential(
+            nn.Linear(self.in_dim,self.out_dim),
+        )
+        self.softmax_layer = nn.Softmax(1)
+
+    def forward(self,x):
+        logits = self.layers(x)
+        probs = self.softmax_layer(logits)
+        return logits, probs
+
+
+def test_classifier():
+    model = IncomeClassifier(105)
+    x = torch.rand([5,105])
+    logits, probs = model(x)
+    import pdb; pdb.set_trace()
+
+
+if __name__=='__main__':
+    test_classifier()
