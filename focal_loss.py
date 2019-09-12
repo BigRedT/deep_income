75067da8b3eafee649b5a53797e7c20650093f8e
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-12 15:25:12 -0500

    add focal_loss

--- focal_loss.py
+++ focal_loss.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class FocalLoss(nn.Module):
-    def __init__(self,gamma):
+    def __init__(self,gamma=1.0):
         super().__init__()
         self.gamma = gamma
     
@@ -11,6 +11,6 @@ class FocalLoss(nn.Module):
         p0 = probs[:,0]
         p1 = probs[:,1]
         loss = \
-            -p1.pow(self.gamma)*labels*torch.log(p1+1e-6) + \
-            -p0.pow(self.gamma)*(1-labels)*torch.log(p0+1e-6)
+            -p0.pow(self.gamma)*labels*torch.log(p1+1e-6) + \
+            -p1.pow(self.gamma)*(1-labels)*torch.log(p0+1e-6)
         return loss.mean()
