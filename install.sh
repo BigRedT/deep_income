4a9b79538037e04c5b38596514b1a5c5f2cd55de
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-12 15:25:30 -0500

    add tensorboard

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 conda create -n income python=3.7
 conda activate income
 conda install pytorch -c pytorch -y
-conda install -c anaconda pyyaml -y
-conda install -c conda-forge python-wget -y
+conda install -c anaconda pyyaml click future -y
+conda install -c conda-forge python-wget tensorboard tensorflow -y
