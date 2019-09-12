908f0c1e7934d6c2f46dcd7c78de527d933ae128
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-12 15:25:47 -0500

    add const for model

--- model.py
+++ model.py
@@ -1,35 +1,59 @@
+import copy
 import torch
 import torch.nn as nn
 
 
+class IncomeClassifierConstants():
+    def __init__(self):
+        self.in_dim = 105
+        self.hidden_dim = 105
+        self.num_hidden_blocks = 2
+        self.drop_prob = 0.2
+        self.out_dim =2
+
+
 class IncomeClassifier(nn.Module):
-    def __init__(self,in_dim,hidden_dim=None):
+    def __init__(self,const):
         super().__init__()
-        self.in_dim = in_dim
-        self.hidden_dim = hidden_dim
-        self.out_dim = 2 
-        # self.layers = nn.Sequential(
-        #     nn.Linear(self.in_dim,self.out_dim),
-        # )
-        self.layers = nn.Sequential(
-            nn.Linear(self.in_dim,self.hidden_dim),
-            nn.BatchNorm1d(self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim,self.hidden_dim),
-            nn.BatchNorm1d(self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim,self.hidden_dim),
-            nn.BatchNorm1d(self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim,self.hidden_dim),
-            nn.BatchNorm1d(self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim,self.hidden_dim),
-            nn.BatchNorm1d(self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim,self.out_dim)
-        )
+        self.const = copy.deepcopy(const)
+
+        if self.const.num_hidden_blocks==0:
+            self.layers = nn.Linear(self.const.in_dim,self.const.out_dim)
+
+        else:
+            layers = []
+            
+            # Add input layers
+            layers.append(self.input_block())
+            
+            # Add hidden layers
+            for i in range(self.const.num_hidden_blocks):
+                layers.append(self.hidden_block())
+            
+            # Add output layers
+            layers.append(self.output_block())
+            
+            self.layers = nn.Sequential(*layers)
+
         self.softmax_layer = nn.Softmax(1)
+
+
+    def input_block(self):
+        return nn.Sequential(
+            nn.Linear(self.const.in_dim,self.const.hidden_dim),
+            nn.BatchNorm1d(self.const.hidden_dim),
+            nn.Dropout(self.const.drop_prob),
+            nn.Sigmoid())
+
+    def hidden_block(self):
+        return nn.Sequential(
+            nn.Linear(self.const.hidden_dim,self.const.hidden_dim),
+            nn.BatchNorm1d(self.const.hidden_dim),
+            nn.Dropout(self.const.drop_prob),
+            nn.Sigmoid())
+
+    def output_block(self):
+        return nn.Linear(self.const.hidden_dim,self.const.out_dim)
 
     def forward(self,x):
         logits = self.layers(x)
@@ -38,7 +62,8 @@ class IncomeClassifier(nn.Module):
 
 
 def test_classifier():
-    model = IncomeClassifier(105)
+    const = IncomeClassifierConstants()
+    model = IncomeClassifier(const)
     x = torch.rand([5,105])
     logits, probs = model(x)
     import pdb; pdb.set_trace()
